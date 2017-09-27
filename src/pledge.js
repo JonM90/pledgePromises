143ada4b72a9b23c4d90cc18327e3d7679447b5a
Jonathan Martinez <jonm90@gmail.com>
2017-09-27 14:13:14 -0400

    ch2

--- src/pledge.js
+++ src/pledge.js
@@ -3,7 +3,31 @@
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
+function $Promise(executor) {
+  if (typeof executor !== 'function') throw TypeError ('executor is not a function.')
+  this._state = 'pending';
+  this._value = null;
 
+  var resolve = this._internalResolve.bind(this);
+  var reject = this._internalReject.bind(this);
+  executor(resolve, reject);
+}
+
+$Promise.prototype._internalResolve = function(data) {
+  //console.log('this._state:', data._state)
+  // this = data;
+  if (this._state === 'pending') {
+    this._state = 'fulfilled';
+    this._value = data;
+  }
+}
+
+$Promise.prototype._internalReject = function(reason) {
+  if (this._state === 'pending') {
+    this._state = 'rejected';
+    this._value = reason;
+  }
+}
 
 
 
